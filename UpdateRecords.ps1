70b978f7dc5d624e0f9347311d5fe9c661948a0b
Justin Johnson <justin@ZIP.AZI>
2025-05-05 08:06:37 -0600

    refining UpdateRecords.ps1

--- UpdateRecords.ps1
+++ UpdateRecords.ps1
@@ -1,24 +1,66 @@
-﻿$noInput = @("", "Texas")
+﻿$noInput = @("")
 
-$statesArr = @("OR", "WA", "CA", "AZ", "NV", "UT", "ID", "MT", "WY", "AR", "LA", "TX", "OK", "CO", "NM", "MI", "WV", "IN", "AL", "VA", "KY", "MS", "OH", "TN", "ME", "MD", "DE", "IL", "FL", "NC", "MA", "RI", "NE", "WI", "MO", "PA", "SD", "NY", "NJ", "KS", "ND", "VT", "NH", "SC", "CT", "MN", "GA", "IA", "AK", "HI")
+$statesArr = @("OR", "DC", "WA", "CA", "AZ", "NV", "UT", "ID", "MT", "WY", "AR", "LA", "TX", "OK", "CO", "NM", "MI", "WV", "IN", "AL", "VA", "KY", "MS", "OH", "TN", "ME", "MD", "DE", "IL", "FL", "NC", "MA", "RI", "NE", "WI", "MO", "PA", "SD", "NY", "NJ", "KS", "ND", "VT", "NH", "SC", "CT", "MN", "GA", "IA", "AK", "HI")
 $correctedStatesMap = @{
+    'Alabama' = 'AL'
+    'Alaska' = 'AK'
+    'Arizona' = 'AZ'
+    'Arkansas' = 'AR'
+    'California' = 'CA'
+    'Colorado' = 'CO'
+    'Connecticut' = 'CT'
+    'Delaware' = 'DE'
+    'District of Columbia' = 'DC'
+    'Florida' = 'FL'
+    'Georgia' = 'GA'
+    'Hawaii' = 'HI'
+    'Idaho' = 'ID'
+    'Illinois' = 'IL'
+    'Indiana' = 'IN'
+    'Iowa' = 'IA'
+    'Kansas' = 'KS'
+    'Kentucky' = 'KY'
+    'Louisiana' = 'LA'
+    'Maine' = 'ME'
+    'Maryland' = 'MD'
+    'Massachusetts' = 'MA'
+    'Michigan' = 'MI'
+    'Minnesota' = 'MN'
+    'Mississippi' = 'MS'
+    'Missouri' = 'MO'
+    'Montana' = 'MT'
+    'Nebraska' = 'NE'
+    'Nevada' = 'NV'
+    'New Hampshire' = 'NH'
+    'New Jersey' = 'NJ'
+    'New Mexico' = 'NM'
+    'New York' = 'NY'
+    'North Carolina' = 'NC'
     'North Dakota' = 'ND'
-    'NB,' = 'NB'
-    '90502' = 'CA'
+    'Ohio' = 'OH'
+    'Oklahoma' = 'OK'
+    'Oregon' = 'OR'
+    'Pennsylvania' = 'PA'
+    'Rhode Island' = 'RI'
+    'South Carolina' = 'SC'
+    'South Dakota' = 'SD'
+    'Tennessee' = 'TN'
+    'Texas' = 'TX'
+    'Utah' = 'UT'
+    'Vermont' = 'VT'
     'Virginia' = 'VA'
     'Washington' = 'WA'
-    #'Hot Springs' = 'AR' #MailingCity needs to Hot Springs, MailingState needs to be AR
-    'Alaska' = 'AK'
-    'NEBRASKA' = 'NE'
-    'Florida' = 'FL'
-    'Kentucky' = 'KY'
-    'Kansas' = 'KS'
+    'West Virginia' = 'WV'
+    'Wisconsin' = 'WI'
+    'Wyoming' = 'WY'
+    
+    # Other inputs to be corrected
+    'NB,' = 'NB'
+    '90502' = 'CA'
     'MA.' = 'MA'
     'IL.' = 'IL'
-    'Oregon' = 'OR'
     'AL.' = 'AL'
     'PA.' = 'PA'
-    
     '74450' = 'OK'
     '18103' = 'PA'
     '40209-9129' = 'KY'
@@ -29,40 +71,21 @@ $correctedStatesMap = @{
     'Fielding' = 'CA'
     'Eagleswood Township' = 'NJ'
     '07711-1093' = 'NJ'
-
     'NJ.' = 'NJ'
     'MI.' = 'MI'
-    'Pennsylvania' = 'PA'
-    'California' = 'CA'
-    'North Carolina' = 'NC'
-    'New York' = 'NY'
-    'Massachusetts' = 'MA'
-    'Missouri' = 'MO'
     'IN.' = 'IN'
     'CA.' = 'CA'
     'ME.' = 'ME'
-    'Colorado' = 'CO'
     'NV,' = 'NV'
-    'Illinois' = 'IL'
     'MD.' = 'MD'
-    'Arizona' = 'AZ'
     'Juneau, AK' = 'AK'
-    'Indiana' = 'IN'
-    'Georgia' = 'GA'
     'ON.' = 'ON'
-    'Idaho' = 'ID'
-    'Louisiana' = 'LA'
-    'South Carolina' = 'SC'
-    'NC 27623' = 'NC'
-    'Texas' = 'TX'
-    'FL.' = 'FL'
-    'CT.' = 'CT'
-    'Maryland' = 'MD'
-    'Ohio' = 'OH'
-    'Connecticut' = 'CT'
-    'New Mexico' = 'NM'
+
     'UT.' = 'UT'
     'MA,' = 'MA'
+    'NC 27623' = 'NC'
+    'FL.' = 'FL'
+    'CT.' = 'CT'
 }
 $statesArrDel = $($statesArr | % {write "'$_'"}) -join ","
 
@@ -87,7 +110,7 @@ foreach($item in $queryData)
 if ($setToArr.Count -gt 1) 
 {
     $setToArr | Export-Csv -Path "contactStateCorrection.csv"
-    sf data update bulk --file "contactStateCorrection.csv" --sobject Contact --async
+    sf data update bulk --file "contactStateCorrection.csv" --sobject Contact -w 10
 }
 
 
